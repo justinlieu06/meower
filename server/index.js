cb30766bcf13f488583fc3fcdc794b462d716e94
JustinLieu06 <justinlieu06@gmail.com>
2020-06-27 23:14:56 -0700

    get request

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Meower!'
+  });
+});
+
 app.listen(5000, () => {
   console.log('Listening on http://localhost:5000');
 });
