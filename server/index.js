6f1099459a41f57a190a8c5951311883c7a83122
JustinLieu06 <justinlieu06@gmail.com>
2020-06-28 21:54:42 -0700

    form validation

--- server/index.js
+++ server/index.js
@@ -12,8 +12,24 @@ app.get('/', (req, res) => {
   });
 });
 
+function isValidMew(mew){
+  return mew.name && mew.name.toString().trim() !== '' &&
+    mew.content && mew.content.toString().trim() !== ''
+}
+
 app.post('/mews', (req, res)=> {
-  console.log(req.body);
+  if (isValidMew(req.body)){
+    //insert into db...
+    const mew = {
+      name: req.body.name.toString(),
+      content: req.body.content.toString()
+    };
+  } else {
+    res.status(422);
+    res.json({
+      message: 'Hey! Name and Content are required!'
+    })
+  }
 });
 
 app.listen(5000, () => {
